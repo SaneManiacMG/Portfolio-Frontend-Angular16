a1a73388ec8e9e5fa2463076a0a422e810ebceb9
SaneManiacMG <mmoroane@hotmail.com>
2023-10-02 19:52:00 +0200

    Imported auth module

--- src/app/authentication/authentication.module.ts
+++ src/app/authentication/authentication.module.ts
@@ -2,16 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import { RegistrationComponent } from './registration/registration.component';
+import { Routes, RouterModule } from '@angular/router';
 
-
+const routes : Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'registration', component: RegistrationComponent },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+]
 
 @NgModule({
-  declarations: [
-    LoginComponent,
-    RegistrationComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [LoginComponent, RegistrationComponent],
+  imports: [CommonModule, RouterModule.forChild(routes)],
 })
-export class AuthenticationModule { }
+export class AuthenticationModule {}
