e74954fdccae161c426fe3b3a2e17a549f67bc35
SaneManiacMG <mmoroane@hotmail.com>
2023-10-09 08:38:32 +0200

    Created Login and Register component

--- src/app/authentication/authentication.component.ts
+++ src/app/authentication/authentication.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LoginComponent } from './login/login.component';
+import { RegistrationComponent } from './registration/registration.component';
 
 @Component({
   selector: 'app-authentication',
   templateUrl: './authentication.component.html',
-  styleUrls: ['./authentication.component.css']
+  styleUrls: ['./authentication.component.css'],
 })
-export class AuthenticationComponent {
+export class AuthenticationComponent implements OnInit {
+  constructor() {}
 
+  ngOnInit(): void {}
+
+  activeTab: string = 'login';
+
+  switchToTab(tabName: string) {
+    console.log('Switching to tab: ' + tabName);
+    this.activeTab = tabName;
+  }
+
+  LoginComponent = LoginComponent;
+  RegistrationComponent = RegistrationComponent;
 }
--- src/app/authentication/authentication.module.ts
+++ src/app/authentication/authentication.module.ts
@@ -4,15 +4,26 @@ import { LoginComponent } from './login/login.component';
 import { RegistrationComponent } from './registration/registration.component';
 import { Routes, RouterModule } from '@angular/router';
 import { AuthenticationComponent } from './authentication.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-const routes : Routes = [
+const routes: Routes = [
+  { path: 'authentication', component: AuthenticationComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'registration', component: RegistrationComponent },
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
-]
+  { path: 'register', component: RegistrationComponent },
+  { path: '', redirectTo: 'authentication', pathMatch: 'full' },
+];
 
 @NgModule({
-  declarations: [LoginComponent, RegistrationComponent, AuthenticationComponent],
-  imports: [CommonModule, RouterModule.forChild(routes)],
+  declarations: [
+    LoginComponent,
+    RegistrationComponent,
+    AuthenticationComponent,
+  ],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes),
+    FormsModule,
+    ReactiveFormsModule,
+  ],
 })
 export class AuthenticationModule {}
